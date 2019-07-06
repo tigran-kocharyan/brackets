ff5bd60fe2637a08422e54fa626db1f9574a3869
Tigran Kocharyan <tigran.kocharyan@outlook.com>
2019-07-06 23:28:30 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let matching = {
+    "[": "]",
+    "(": ")",
+    "{": "}",
+    "|" : "|"
+  }
+
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(" || str[i] === "{" || str[i] === "[" || str[i] === "|") {
+
+      if (str[i] === "|" && count === 0) {
+        stack.push(str[i]);
+        count++;
+      }
+
+      if (str[i] === "|" && count > 0) {
+        check = stack.pop();
+
+        if (str[i] !== "|") {
+          return false;
+        }
+        count = count - 1;
+      }
+      if (str[i] !== "|")
+        stack.push(str[i]);
+    }
+
+    else {
+      check_last_char = stack.pop();
+
+      if (str[i] !== matching[check_last_char]) {
+        return false;
+      }
+    }
+  }
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
